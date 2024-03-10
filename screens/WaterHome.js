01bf385501ed082b78b2226eed03f0bd148f2764
Ark-01-2043 <anh12310b1111@gmail.com>
2024-03-10 11:30:40 +0700

    hello world

--- screens/WaterHome.js
+++ screens/WaterHome.js
@@ -1,7 +1,7 @@
-import { ScrollView } from "react-native";
+import { ScrollView, Text } from "react-native";
 
 export default function WaterHome(props) {
     return <ScrollView>
-        
+        <Text>Hello world</Text>
     </ScrollView>
 };
